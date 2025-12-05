08965a3065d564ddbfafc68ce5ed861a49273611
Yijia Zhang <yijiazzz@google.com>
2025-12-05 17:37:15 +0000

    Update google-cloud-chat version and enforce PORT 8080

--- src/main/java/com/google/chat/bot/Application.java
+++ src/main/java/com/google/chat/bot/Application.java
@@ -6,6 +6,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
+    String port = System.getenv("PORT");
+    if (port == null) {
+      port = "8080";
+    }
+    System.setProperty("server.port", port);
     SpringApplication.run(Application.class, args);
   }
 }
