87e25cb036c4dbc10a4410451ef234afe71c7ddf
Yijia Zhang <yijiazzz@google.com>
2025-12-09 18:57:11 +0000

    update card

--- src/main/java/com/google/chat/bot/Application.java
+++ src/main/java/com/google/chat/bot/Application.java
@@ -6,11 +6,18 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Application {
   public static void main(String[] args) {
-    String port = System.getenv("PORT");
-    if (port == null) {
-      port = "8080";
+    System.out.println("Starting Application...");
+    try {
+      String port = System.getenv("PORT");
+      if (port == null) {
+        port = "8080";
+      }
+      System.setProperty("server.port", port);
+      SpringApplication.run(Application.class, args);
+    } catch (Throwable t) {
+      System.err.println("Application startup failed:");
+      t.printStackTrace();
+      throw t;
     }
-    System.setProperty("server.port", port);
-    SpringApplication.run(Application.class, args);
   }
 }
